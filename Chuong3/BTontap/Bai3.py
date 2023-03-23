be08dbb046d7f35d4774468bddc895681499ae90
tranletuanh2801 <tltua2801@gmail.com>
2023-03-23 10:49:03 +0700

    anh1

---
+++ Chuong3/BTontap/Bai3.py
@@ -0,0 +1,7 @@
+
+n=int(input("n="))
+i=0
+while n>=1:
+    i=i+1
+    n//=10
+print(n, "co ",i ,"chu so")
--- Chuong3/BTontap/Bai4.py
+++ Chuong3/BTontap/Bai4.py
@@ -1,21 +1,27 @@
-a=float(input("a="))
-b=float(input("b="))
-ch=input("Toan tu:")
-if ch=="+":
-    x=a+b
-    print(a,ch,b,"=",x,sep="")
-elif ch=="-":
-    x=a-b
-    print(a,ch,b,"=",x,sep="")
-elif ch=="*":
-    x=a*b
-    print(a,ch,b,"=",x,sep="")
-elif ch=="/":
-    x=a/b
-    print(a,ch,b,"=",x,sep="")
-tiep_tuc=input("Tiep tuc:")
 while True:
-    if tiep_tuc=="t" or "T":
+    a=float(input("a="))
+    b=float(input("b="))
+    ch=input("Toan tu:")
+    if ch=="+":
+        x=a+b
+        print(a,ch,b,"=",x,sep="")
+    elif ch=="-":
+        x=a-b
+        print(a,ch,b,"=",x,sep="")
+    elif ch=="*":
+        x=a*b
+        print(a,ch,b,"=",x,sep="")
+    elif ch=="/":
+        if a==0 or b==0:
+            print("Khong hop le")
+        else:
+            x=a/b
+            print(a,ch,b,"=",x,sep="")
+    else:
+        print("Khong hop le")
+        continue
+    tiep_tuc=input("Tiep tuc:")
+    if tiep_tuc=="t" or tiep_tuc=="T":
         break
     
     
