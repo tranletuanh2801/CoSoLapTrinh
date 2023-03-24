b3ff2ffe3335a7496914a99d51660a4cfc14f4f4
tranletuanh2801 <tltua2801@gmail.com>
2023-03-24 21:00:57 +0700

    anh1

--- Chuong3/BTontap/Bai2.py
+++ Chuong3/BTontap/Bai2.py
@@ -1,5 +1,27 @@
+# n=int(input("n="))
+# if n%2==1 and n/n==1:
+#     print(n,"la SNT")
+# else:
+#     print(n,"khong la SNT")
 n=int(input("n="))
-if n%2==1 and n/n==1:
-    print(n,"la SNT")
-else:
-    print(n,"khong la SNT")
+# if n==2:
+#     print(n,"la SNT")
+if n>=2 and n<=100: #if n>2 and n<=100:
+    for i in range(2,n):
+        if n%i==0:
+            print(n,"khong la SNT")
+            break
+    else:
+        print(n,"la SNT")
+#c3
+# n=int(input())
+# SNT=True #Biến cờ
+# for i in range (2,n):
+#     if n%i==0:
+#         SNT=False
+#         break
+# if  SNT==True:
+#     print(n,'la SNT')
+# else:
+#     print(n,'khong la SNT')
+
