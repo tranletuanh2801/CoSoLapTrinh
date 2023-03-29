489a17c6ef56b936dca82d257d8862081e8de999
tranletuanh2801 <tltua2801@gmail.com>
2023-03-29 20:11:45 +0700

    anh1

--- Chuong3/BTCNwhile_for/Ex3.7.b.py
+++ Chuong3/BTCNwhile_for/Ex3.7.b.py
@@ -1,15 +1,15 @@
-n=int(input())
-
-s=1
-if n==0 or n==1:
-    s=1
-    print(s)
-else:
-    for i in range(2,n+1):
-        s=s*i
+while True:
+    n=int(input())
+    if n>=0:
+        s=1
+        for i in range(1,n+1):
+            s=s*i
         print(s)
-        if n<0:
-            break
+    else :
+        break
+    
+
+
 
 
    
