9b86880f0bce4156e5746adef7f060b8ec027f9e
tranletuanh2801 <tltua2801@gmail.com>
2023-03-30 12:24:08 +0700

    anh1

---
+++ Chuong4/BTN/Ex84.py
@@ -0,0 +1,17 @@
+
+# def trung_binh(a, b, c):
+#   return (a + b + c) / 3
+    
+
+  
+
+# a = float(input("Nhập số thứ nhất: "))
+# b = float(input("Nhập số thứ hai: "))
+# c = float(input("Nhập số thứ ba: "))
+
+# tb = trung_binh(a, b, c)
+# print(f"Giá trị trung bình của {a}, {b} và {c} là: {tb}")
+
+
+    
+    
