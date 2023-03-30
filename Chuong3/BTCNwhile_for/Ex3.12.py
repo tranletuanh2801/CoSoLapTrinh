04e89fcba98a157b263fff26b2a3ac938f68f12f
tranletuanh2801 <tltua2801@gmail.com>
2023-03-30 12:24:15 +0700

    anh1

--- Chuong3/BTCNwhile_for/Ex3.12.py
+++ Chuong3/BTCNwhile_for/Ex3.12.py
@@ -1,9 +1,39 @@
+# n=input()
+# m={"0":"A","1":"B","2":"C","3":"D","4":"E","5":"F","6":"G","7":"H","8":"K","9":"L"}
+# ketqua=""
+# for so in n:
+#     if so in m:
+#         ketqua+=m[so]
+# print(ketqua)
 n=input()
-m={"0":"A","1":"B","2":"C","3":"D","4":"E","5":"F","6":"G","7":"H","8":"K","9":"L"}
-ketqua=""
-for so in n:
-    if so in m:
-        ketqua+=m[so]
-print(ketqua)
+m=''
+for i in n:
+    if i=="0":
+        m+="A"
+    elif i=="1":
+        m+="B"
+    elif i=="2":
+        m+="C"
+    elif i=="3":
+        m+="D"
+    elif i=="4":
+        m+="E"
+    elif i=="5":
+        m+="F"
+    elif i=="6":
+        m+="G"
+    elif i=="7":
+        m+="H"
+    elif i=="8":
+        m+="K"
+    elif i=="9":
+        m+="L"
+print(m) 
+        
+        
+    
+        
+        
+
 
 
