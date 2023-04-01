1bc18fcb7fe25493788749b626a7c35f7351be8a
tranletuanh2801 <tltua2801@gmail.com>
2023-04-01 23:49:36 +0700

    anh1

--- Luyencode/b1.py
+++ Luyencode/b1.py
@@ -1,19 +1,16 @@
 
-
-            
-
-Lenh=input()
-if Lenh=='tg':
-    a=float(input())
-    b=float(input())
-    c=float(input())
-    Stg=a+b+c
-    print(Stg)
-elif Lenh== 'ht':
-    bk=float(input())
-    pi=3.14
-    Sht=2*bk*pi
-    print(Sht)
+# Lenh=input()
+# if Lenh=='tg':
+#     a=float(input())
+#     b=float(input())
+#     c=float(input())
+#     Stg=a+b+c
+#     print(Stg)
+# elif Lenh== 'ht':
+#     bk=float(input())
+#     pi=3.14
+#     Sht=2*bk*pi
+#     print(Sht)
 
 
 # w= int(input())
@@ -52,4 +49,26 @@ elif Lenh== 'ht':
 # if a<=b and a>0 and a<=tong<b:
 #     print(a+b)
 # else:
-#     print("0")                
+#     print("0")   
+
+# for i in range(0,10):
+#     for j in range(i+1,101,10):
+#         print("{:<3}".format(j),end=" ")
+#     print()
+
+# n=int(input("n="))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("",end=" ")
+#     for j in range(1,n+2-i):
+#         print("*",end=" ")
+#     print() 
+     
+     
+n=int(input("n="))
+for i in range(1,n+1):
+    for j in range(1,n+1-i):
+        print("",end=" ")
+    for k in range(1,i+1):
+        print("*",end=" ")
+    print()
---
+++ Luyencode/b4.py
@@ -0,0 +1,6 @@
+r=float(input())
+Pi=3.14
+# cvi=round(2*Pi*r,3)
+# s=round(Pi*r**2,3)
+print(round(2*Pi*r,5),end=" ")
+print(round(Pi*r**2,5))
