e227bf43da4099d2bdbb35e26b9e32c6fb65bb48
tranletuanh2801 <tltua2801@gmail.com>
2023-04-01 21:39:27 +0700

    anh1

--- Chuong3/BTCNwhile_for/Ex3.10.c.py
+++ Chuong3/BTCNwhile_for/Ex3.10.c.py
@@ -3,3 +3,13 @@ for i in range(1,n+1):
     for j in range(1,n+1):
         print(j,end=" ")
     print()
+
+# n=int(input("n="))
+# i=0
+# while i<n:
+#     j=1
+#     while j<=n:
+#         print(j,end=" ")
+#         j+=1
+#     print()
+#     i+=1
