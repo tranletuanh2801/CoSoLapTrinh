e470c8374fd4775685d9576fcdb07592350bd8c0
tranletuanh2801 <tltua2801@gmail.com>
2023-04-02 07:57:18 +0700

    anh1

---
+++ Luyencode/bai.py
@@ -0,0 +1,19 @@
+# i=1
+# while i<=9:
+#     print(" "*(9-i),end="")
+#     print("*"*(2*i-1))
+#     i+=1   tam giác
+    
+# i=9
+# while i>=1:
+#     print("*"*(2*i-1))
+#     print(" "*(9-i),end="")
+#     i-=1 lỗi tam giác ngược
+
+# n=int(input("n="))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("",end=" ")
+#     for j in range(1,n+2-i):
+#         print("*",end=" ")
+#     print()  tam giác ngược
