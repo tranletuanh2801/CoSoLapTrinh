b9c18d60be5191b4e8efa9a2b5c892122bb7f491
tranletuanh2801 <tltua2801@gmail.com>
2023-04-13 17:00:37 +0700

    anh1

--- Chuong4/BTN/Nhom7/Ex82.py
+++
@@ -1,9 +0,0 @@
-def tinh_gia_tien(distance):
-    gia_goc = 4.0
-    don_gia = 0.25
-    so_don_vi = distance / 140
-    gia_tien = gia_goc + don_gia * so_don_vi
-    return round(gia_tien, 2)
-distance = float(input("Nhap khoang cach di chuyen (tinh bang met): "))
-print("Tong chi phi la:", tinh_gia_tien(distance),"$",sep=" ")
-
