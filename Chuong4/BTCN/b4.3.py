92bcc9955bbdeb6f549a9af532ce64b3367c60ba
tranletuanh2801 <tltua2801@gmail.com>
2023-04-14 10:46:24 +0700

    anh1

--- Chuong4/BTCN/b4.3.py
+++ Chuong4/BTCN/b4.3.py
@@ -8,30 +8,22 @@ def nhap():
 def giaipt(a,b,c):
     d=b*b-4*a*c
     if d<0:
-        return ""
-        # print("Phuong trinh vo nghiem")
+        print("Phuong trinh vo nghiem")
+        #return None
     elif d==0:
         x1=(-b/2*a)
-        return x1
-        # print("Phuong trinh co nghiem kep")
-        # print("x1=x2=",x,sep="")
+        print("Phuong trinh co nghiem kep")
+        print("x1=x2=",x1,sep="")
+        #return x1
     else:
         x1=(-b+math.sqrt(d))/2*a
         x2=(-b-math.sqrt(d))/2*a
-        return x1,x2
-        # print("Phuong trinh co hai nghiem")
-        # print("x1=",x1,sep="")
-        # print("x2=",x2,sep="")
-def inkq(x1,x2):
-    if giaipt(a,b,c):
-        print("Phuong trinh vo nghiem")
-    elif giaipt(a,b,c):
-        print("Phuong trinh co nghiem kep")
-        print("x1=x2=",x1,sep="")
-    else:
         print("Phuong trinh co hai nghiem")
         print("x1=",x1,sep="")
         print("x2=",x2,sep="")
+        #return x1,x2
+def inkq(x1,x2):
+    print(x1,x2)
 a,b,c=nhap()
-x1,x2=giaipt(a,b,c)
-inkq(x1,x2)
+giaipt(a,b,c)
+
