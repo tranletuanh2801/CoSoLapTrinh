412af0f42418f5e14dd283633063d0964f57fb8d
tranletuanh2801 <tltua2801@gmail.com>
2023-04-21 15:48:06 +0700

    anh1

--- Chuong5/Slide/Bai1.py
+++ Chuong5/Slide/Bai1.py
@@ -1,17 +1,25 @@
+# Nhập từ bàn phím 2 số nguyên x, k;
+# Nhập một số nguyên n và n số nguyên lưu vào list L;
+# Hàm add(L, x, k) thêm phần tử x vào List L tại vị trí index k, nếu
+# k lớn hơn số phần tử của L thì thêm x vào cuối L;
+def Nhap():
+    x=int(input())
+    k=int(input())
+    n=int(input())
+    L=[]
+    for i in range(n):
+        a=int(input())
+        L=L+[a]
+    return L,x,k   
 def add(L,x,k):
-    if k>len(L):
-        L=L+[x]
-    elif k==0:
-        L=[x]+L
-    else: #chèn vào vị trí k
-        L=L[:k]+[x]+L[k:]
-    return L
-L=[1,2,3,4,5,6,7]
+    # if k>len(L):
+    #     L=L+[x]
+    # elif k==0:
+    #     L=[x]+L
+    # else: 
+    L=L[:k]+[x]+L[k:]
+    print(L)
+L,x,k=Nhap()
+add(L,x,k)
 
-L=add(L,10,0)
-print(L)
-L=add(L,15,3)
-print(L)
-L=add(L,20,100)
-print(L)
 
--- Chuong5/Slide/Bai4.py
+++ Chuong5/Slide/Bai4.py
@@ -1,8 +1,17 @@
+# Nhập một số nguyên n và n số nguyên lưu vào list L
+# Hàm count(L) trả về số lượng phần tử trong List L;
+def Nhap():   
+    n=int(input())
+    L=[]
+    for i in range(n):
+        a=int(input())
+        L=L+[a]
+    return L
 def count(L):
     dem=0
     for i in L:
-        dem+=1
-    return L
+        dem=dem+1
+    print(dem)
+L=Nhap()
+count(L)
 
-L=[1,5,2,3,4,5]
-print(L.count(5))
